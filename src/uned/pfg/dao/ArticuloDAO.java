77795cda87d0852cbd9d6a5b36fdfcf0131b94d4
jorgestp <jorge10_stp@hotmail.com>
2019-05-24 10:40:36 +0200

    Se crean los comentarios JavaDoc de la clase ArticuloDAO.

--- src/uned/pfg/dao/ArticuloDAO.java
+++ src/uned/pfg/dao/ArticuloDAO.java
@@ -27,48 +27,66 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import uned.pfg.bean.Articulo;
-import uned.pfg.bean.ArticuloPedido;
 import uned.pfg.ws.PoolConexiones;
 
+/**
+ * Clase que representa el Objeto de Acceso a Datos almacenados de los Articulos
+ * del sistema.
+ * 
+ * Se hacen las pertinentes consultas de actualizacion, inserccion o borrado de
+ * todo lo referente a los articulos que tiene el sistema y que se detallarán en
+ * las funciones correspondientes.
+ * 
+ * 
+ * @author JORGE VILLALBA RUIZ 47536486V
+ * @version 1.0
+ */
 public class ArticuloDAO {
 
-	
 	private DataSource origendatos;
 	private final String FILENAME_ARTICULOS = "XML_articulos.xml";
 	private final String FILENAME_ARTICULO_selec = "XML_articulo_selec";
+
+	/**
+	 * Constructor que inicializa la variable pasada por parametros con el pool de
+	 * conexiones establecido para la base de datos, y ademas asigna esta variable
+	 * al campo de clase correspondiente al DataSource
+	 * 
+	 * @param origendatos Objeto que representa el pool de conexiones y el que es
+	 *                    inicializado y luego asignado al campo de clase
+	 */
 	public ArticuloDAO(DataSource origendatos) {
-		
+
 		origendatos = PoolConexiones.getInstance().getConnection();
-		
+
 		this.origendatos = origendatos;
-		
+
 	}
-	
-	
-	public List<Articulo> getArticulos(){
-		
-		
+
+	/**
+	 * Funcion que consulta a la BBDD todos los articulos que hay en el sistema
+	 * 
+	 * @return Lista con todos los articulos que hay en el sistema.
+	 */
+	public List<Articulo> getArticulos() {
+
 		List<Articulo> articulos = new ArrayList<Articulo>();
 		Connection conexion = null;
-		Statement state=null;
-		ResultSet rs =null;
-		
+		Statement state = null;
+		ResultSet rs = null;
+
 		try {
-			
-		
-			
+
 			conexion = origendatos.getConnection();
-			
-			
-			
+
 			String consulta = "SELECT * FROM ARTICULO";
-			
+
 			state = conexion.createStatement();
-			
-			rs=state.executeQuery(consulta);
-			
-			while(rs.next()) {
-				
+
+			rs = state.executeQuery(consulta);
+
+			while (rs.next()) {
+
 				int id_articulo = rs.getInt(1);
 				String nombre = rs.getString(2);
 				Date fecha = rs.getDate(3);
@@ -76,24 +94,29 @@ public class ArticuloDAO {
 
 				articulos.add(new Articulo(id_articulo, nombre, fecha, precio));
 			}
-			
+
 			state.close();
 			conexion.close();
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 
 			e.printStackTrace();
 		}
-		
-		
+
 		return articulos;
-		
-		
+
 	}
-	
+
+	/**
+	 * Funcion que tiene por objetivo introducir un articulo en la BBDD.
+	 * 
+	 * @param a Bean que corresponde al articulo que se quiere introducir en el
+	 *          sistema.
+	 * @return True, si la inserccion ha sido correcta, o false, si ha habido algun
+	 *         problema de inserccion
+	 */
 	public boolean introducirArticulo(Articulo a) {
-		
-		
+
 		Connection conexion = null;
 		PreparedStatement state = null;
 
@@ -101,21 +124,18 @@ public class ArticuloDAO {
 
 			conexion = origendatos.getConnection();
 
-			String sql = "INSERT INTO ARTICULO (nombre, "
-					+ "fecha_entrada, precio) "
-					+ "VALUES ( ?,?,?)";
+			String sql = "INSERT INTO ARTICULO (nombre, " + "fecha_entrada, precio) " + "VALUES ( ?,?,?)";
 
 			state = conexion.prepareStatement(sql);
 
 			state.setString(1, a.getNombre());
-	
+
 			java.util.Date f = a.getFecha_entrada();
 
 			java.sql.Date fecha = new java.sql.Date(f.getTime());
 
 			state.setDate(2, fecha);
 			state.setDouble(3, a.getPrecio());
-
 
 			state.execute();
 
@@ -129,14 +149,19 @@ public class ArticuloDAO {
 			return false;
 		}
 	}
-	
-	
-	
+
+	/**
+	 * Funcion que, dado un identificador de articulo, devuelve dicho articulo
+	 * haciendo una consulta a la BBDD
+	 * 
+	 * @param id Entero representativo al id del articulo
+	 * @return Bean correspondiente al Articulo que andamos buscando
+	 */
 	public Articulo SeleccionarArticulo(int id) {
-		
+
 		Connection conexion = null;
 		PreparedStatement state = null;
-		ResultSet rs =null;
+		ResultSet rs = null;
 		Articulo a = null;
 
 		try {
@@ -148,164 +173,173 @@ public class ArticuloDAO {
 			state = conexion.prepareStatement(sql);
 
 			state.setInt(1, id);
-	
+
 			rs = state.executeQuery();
-			
-			while(rs.next()) {
-				
+
+			while (rs.next()) {
+
 				a = new Articulo(id, rs.getString(2), rs.getDate(3), rs.getDouble(4));
 			}
 
 			state.close();
 			conexion.close();
-			
+
 			return a;
 
 		} catch (Exception e) {
 
 			e.printStackTrace();
-			
+
 		}
-		
+
 		return a;
-		
+
 	}
-	
+
+	/**
+	 * Crea un xml usando el parser DOM de Java, en el que se recorre el objeto
+	 * lista pasado por parametro y se va construyendo el archivo xml. Finalmente,
+	 * ese archivo es transformado en cadena de carateres
+	 * 
+	 * @param listaArticulos Lista que representa a los articulos que hay en el
+	 *                       sistema y los que se quiere parsear a XML
+	 * @return Cadena de caracteres que representa al xml creado en el archivo
+	 */
+	@SuppressWarnings("resource")
 	public String crearXML_Articulos(List<Articulo> listaArticulos) {
-		
-		
-		 String s = "";
-	        String line;
-	          
-	      try{
-	           
-	              DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	              DocumentBuilder builder = factory.newDocumentBuilder();
-	              Document document = builder.newDocument();
-	              
-	              Element root = document.createElement("articulos");
-	              document.appendChild(root);
-	              
-	              Iterator<Articulo> it = listaArticulos.iterator();
-	              
-	              while(it.hasNext()){
-	                  
-	            	  Articulo p = it.next();
-	                  
-	                  Element art = document.createElement("articulo");
-	                  root.appendChild(art);
-	                 
-	                  Element id_articulo = document. createElement("id_articulo");
-	                  art.appendChild(id_articulo);
-	                  id_articulo.appendChild(document.createTextNode(String.valueOf(p.getId_articulo())));
-	                  
-	                  
-	                  Element nombre = document.createElement("nombre");
-	                  art.appendChild(nombre);
-	                  nombre.appendChild(document.createTextNode(String.valueOf(p.getNombre())));
-	   
-	                  Element fecha_entrada = document.createElement("fecha_entrada");
-	                  art.appendChild(fecha_entrada);
-	                  fecha_entrada.appendChild(document.createTextNode(String.valueOf(p.getFecha_entrada())));
-	                  
-	                  Element precio = document.createElement("precio");
-	                  art.appendChild(precio);
-	                  precio.appendChild(document.createTextNode(String.valueOf(p.getPrecio())));
-	                  
-            
-	              }
-	              
-	              
-	              TransformerFactory tFactory = TransformerFactory.newInstance();
-	              Transformer transformer = tFactory.newTransformer();
-	              DOMSource source = new DOMSource(document);
-	              StreamResult result = new StreamResult(new File(FILENAME_ARTICULOS));
 
-	              transformer.transform(source, result);
-	              
-	              File ar = new File(FILENAME_ARTICULOS);
-	              FileReader f = new FileReader(ar);
-	              BufferedReader b = new BufferedReader(f); 
-	              while((line = b.readLine())!=null) {
-	                  s= s + line +"\n";
-	                  
-	              } 
-	              
-	              System.out.println(ar.getAbsolutePath());
-	              
-	          }catch( IOException | ParserConfigurationException | TransformerException | DOMException e){
-	              
-	              e.printStackTrace();
-	          }
-	     
-	          
-	          return s;
+		String s = "";
+		String line;
+
+		try {
+
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document document = builder.newDocument();
+
+			Element root = document.createElement("articulos");
+			document.appendChild(root);
+
+			Iterator<Articulo> it = listaArticulos.iterator();
+
+			while (it.hasNext()) {
+
+				Articulo p = it.next();
+
+				Element art = document.createElement("articulo");
+				root.appendChild(art);
+
+				Element id_articulo = document.createElement("id_articulo");
+				art.appendChild(id_articulo);
+				id_articulo.appendChild(document.createTextNode(String.valueOf(p.getId_articulo())));
+
+				Element nombre = document.createElement("nombre");
+				art.appendChild(nombre);
+				nombre.appendChild(document.createTextNode(String.valueOf(p.getNombre())));
+
+				Element fecha_entrada = document.createElement("fecha_entrada");
+				art.appendChild(fecha_entrada);
+				fecha_entrada.appendChild(document.createTextNode(String.valueOf(p.getFecha_entrada())));
+
+				Element precio = document.createElement("precio");
+				art.appendChild(precio);
+				precio.appendChild(document.createTextNode(String.valueOf(p.getPrecio())));
+
+			}
+
+			TransformerFactory tFactory = TransformerFactory.newInstance();
+			Transformer transformer = tFactory.newTransformer();
+			DOMSource source = new DOMSource(document);
+			StreamResult result = new StreamResult(new File(FILENAME_ARTICULOS));
+
+			transformer.transform(source, result);
+
+			File ar = new File(FILENAME_ARTICULOS);
+			FileReader f = new FileReader(ar);
+			BufferedReader b = new BufferedReader(f);
+			while ((line = b.readLine()) != null) {
+				s = s + line + "\n";
+
+			}
+
+			// System.out.println(ar.getAbsolutePath());
+			ar.delete();
+
+		} catch (IOException | ParserConfigurationException | TransformerException | DOMException e) {
+
+			e.printStackTrace();
+		}
+
+		return s;
 	}
-	
+
+	/**
+	 * Crea un xml usando el parser DOM de Java, en el que se recorre el objeto
+	 * pasado por parametro y se va construyendo el archivo xml. Finalmente, ese
+	 * archivo es transformado en cadena de carateres
+	 * 
+	 * @param a Bean del objeto articulo que se quiere crear el xml
+	 * @return Cadena de carateres que representa al contenido del archivo xml
+	 *         creado.
+	 */
+	@SuppressWarnings("resource")
 	public String crearXML_artSelec(Articulo a) {
-		
-		
-		 String s = "";
-	        String line;
-	          
-	      try{
-	           
-	              DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	              DocumentBuilder builder = factory.newDocumentBuilder();
-	              Document document = builder.newDocument();
-	              
-	              Element root = document.createElement("articulo_selec");
-	              document.appendChild(root);
 
-	                  
-	                  Element art = document.createElement("articulo");
-	                  root.appendChild(art);
-	                 
-	                  Element id_articulo = document. createElement("id_articulo");
-	                  art.appendChild(id_articulo);
-	                  id_articulo.appendChild(document.createTextNode(String.valueOf(a.getId_articulo())));
-	                  
-	                  
-	                  Element nombre = document.createElement("nombre");
-	                  art.appendChild(nombre);
-	                  nombre.appendChild(document.createTextNode(String.valueOf(a.getNombre())));
-	   
-	                  Element fecha_entrada = document.createElement("fecha_entrada");
-	                  art.appendChild(fecha_entrada);
-	                  fecha_entrada.appendChild(document.createTextNode(String.valueOf(a.getFecha_entrada())));
-	                  
-	                  Element precio = document.createElement("precio");
-	                  art.appendChild(precio);
-	                  precio.appendChild(document.createTextNode(String.valueOf(a.getPrecio())));
-	                  
-           
-	              
-	              
-	              
-	              TransformerFactory tFactory = TransformerFactory.newInstance();
-	              Transformer transformer = tFactory.newTransformer();
-	              DOMSource source = new DOMSource(document);
-	              StreamResult result = new StreamResult(new File(FILENAME_ARTICULO_selec));
+		String s = "";
+		String line;
 
-	              transformer.transform(source, result);
-	              
-	              File ar = new File(FILENAME_ARTICULO_selec);
-	              FileReader f = new FileReader(ar);
-	              BufferedReader b = new BufferedReader(f); 
-	              while((line = b.readLine())!=null) {
-	                  s= s + line +"\n";
-	                  
-	              } 
-	              
-	              System.out.println(ar.getAbsolutePath());
-	              
-	          }catch( IOException | ParserConfigurationException | TransformerException | DOMException e){
-	              
-	              e.printStackTrace();
-	          }
-	     
-	          
-	          return s;
+		try {
+
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document document = builder.newDocument();
+
+			Element root = document.createElement("articulo_selec");
+			document.appendChild(root);
+
+			Element art = document.createElement("articulo");
+			root.appendChild(art);
+
+			Element id_articulo = document.createElement("id_articulo");
+			art.appendChild(id_articulo);
+			id_articulo.appendChild(document.createTextNode(String.valueOf(a.getId_articulo())));
+
+			Element nombre = document.createElement("nombre");
+			art.appendChild(nombre);
+			nombre.appendChild(document.createTextNode(String.valueOf(a.getNombre())));
+
+			Element fecha_entrada = document.createElement("fecha_entrada");
+			art.appendChild(fecha_entrada);
+			fecha_entrada.appendChild(document.createTextNode(String.valueOf(a.getFecha_entrada())));
+
+			Element precio = document.createElement("precio");
+			art.appendChild(precio);
+			precio.appendChild(document.createTextNode(String.valueOf(a.getPrecio())));
+
+			TransformerFactory tFactory = TransformerFactory.newInstance();
+			Transformer transformer = tFactory.newTransformer();
+			DOMSource source = new DOMSource(document);
+			StreamResult result = new StreamResult(new File(FILENAME_ARTICULO_selec));
+
+			transformer.transform(source, result);
+
+			File ar = new File(FILENAME_ARTICULO_selec);
+			FileReader f = new FileReader(ar);
+			BufferedReader b = new BufferedReader(f);
+			while ((line = b.readLine()) != null) {
+				s = s + line + "\n";
+
+			}
+
+			// System.out.println(ar.getAbsolutePath());
+			ar.delete();
+
+		} catch (IOException | ParserConfigurationException | TransformerException | DOMException e) {
+
+			e.printStackTrace();
+		}
+
+		return s;
 	}
-   
+
 }
