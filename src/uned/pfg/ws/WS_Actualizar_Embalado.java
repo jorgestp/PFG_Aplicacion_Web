e3a923f9e709e2cf4e1f1c681101dba4e30d9a56
jorgestp <jorge10_stp@hotmail.com>
2019-05-24 11:57:37 +0200

    Se crea los comentarios de JavaDoc de los dos primeros WS.

--- src/uned/pfg/ws/WS_Actualizar_Embalado.java
+++ src/uned/pfg/ws/WS_Actualizar_Embalado.java
@@ -4,12 +4,23 @@ import org.apache.commons.dbcp.BasicDataSource;
 
 import uned.pfg.dao.PedidoDAO;
 
+/**
+ * Clase que representa a un Web Service que tiene como funcion actualizar el valor
+ * de Embalado de un articulo en un pedido concreto
+ *
+ * @author JORGE VILLALBA RUIZ 47536486V
+ * @version 1.0
+ */
 public class WS_Actualizar_Embalado {
 	
 
 	private PedidoDAO pedidoDAO;
 	private BasicDataSource basicDataSource;
 	
+	/**
+	 * Construtor por defecto, que llama al pool de conexiones ( si es que estaba ya creado, si no
+	 * lo crea) y pasa este pool de conexiones al constructor del DAO de pedidos.
+	 */
 	public WS_Actualizar_Embalado() {
 
 
@@ -17,6 +28,16 @@ public class WS_Actualizar_Embalado {
 		pedidoDAO = new PedidoDAO(basicDataSource);
 	}
 
+	/**
+	 * Funcion que se encargar de llamar al objeto DAO correspondiente para actualiar el valor
+	 * de embalado de el articulo en el pedido concreto.
+	 * 
+	 * @param id_pedido Entero que representa el id de un pedido concreto
+	 * @param id_articulo Entero que representa el id de un articulo.
+	 * @return String con valor "exito", en caso de actualizarse correctamente el valor embalado,
+	 * o "error", en caso de ocurrir algun error que impida la acutalizacion como el caso de caida del 
+	 * servidor, o de la conexion con la base de datos
+	 */
 	public String actualizaEmbalado(String id_pedido, String id_articulo) {
 		
 		boolean flag = pedidoDAO.actualizarEmbalado(Integer.parseInt(id_pedido), Integer.parseInt(id_articulo));
--- src/uned/pfg/ws/WS_Almacen.java
+++ src/uned/pfg/ws/WS_Almacen.java
@@ -5,18 +5,33 @@ import org.apache.commons.dbcp.BasicDataSource;
 import uned.pfg.dao.AlmacenDAO;
 
 
-
+/**
+ * Clase que representa a un WEB SERVICE que tiene como funcion obetener los articulos
+ * qie hay en el alamcen en un momento determinado
+ *
+ * @author JORGE VILLALBA RUIZ 47536486V
+ * @version 1.0
+ */
 public class WS_Almacen {
 
 	private BasicDataSource basicDataSource;
 	private AlmacenDAO almacenDAO;
 	
+	/**
+	 * Construtor por defecto, que llama al pool de conexiones ( si es que estaba ya creado, si no
+	 * lo crea) y pasa este pool de conexiones al constructor del DAO de almacen.
+	 */
 	public WS_Almacen() {
 
 		basicDataSource = PoolConexiones.getInstance().getConnection();
 		almacenDAO = new AlmacenDAO(basicDataSource);
 	}
 	
+	/**
+	 * Funcion que consulta todos los articulos que hay en el almacen, creando un xml con ellos
+	 * y parseandolos a String para su envio mediante Web Service
+	 * @return String que representa el xml con todos los articulo del almacen
+	 */
 	public String enviarAlmacen() {
 		
 		
